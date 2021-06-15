bdc35f94c0116509e869857b35991e35e3a5a562
BS-595 <BS-595@192.168.0.102>
2021-06-15 16:45:52 +0600

    **Selenium Commit Message**

---
+++ First project_Demo/src/learning_java_package/HelloWorld.java
@@ -0,0 +1,13 @@
+package learning_java_package;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		System.out.println("Hello Liton");
+		
+	}
+
+}
